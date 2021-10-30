39db9451de5fe7b895f986c40f583c25f2dd89e6
ak <theachyutkadam@gmail.com>
2021-10-30 13:30:43 +0530

    create user model with polymorphic association

---
+++ db/migrate/20211030080023_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :user_name
+      t.string :password
+      t.string :first_name
+      t.string :last_name
+      t.string :contact
+      t.text :address
+      t.string :gender
+      t.references :role, null: false, foreign_key: true
+      t.integer :status
+      t.references :information, polymorphic: true, null: false
+
+      t.timestamps
+    end
+  end
+end
